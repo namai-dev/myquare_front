ea14908c184c5531491920e50fab3c47c43cc89a
namai-dev <namaiHarmony4@gmail.com>
2024-01-03 08:05:06 +0300

    login/signup

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,15 +1,32 @@
+import axios from "axios";
 import React, { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AuthContext = createContext();
 
 export default AuthContext;
 
 export const AuthProvider = ({ children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    localStorage.getItem("authtoken") != null ? true : false
+  );
+  const baseurl = "http://127.0.0.1:8001";
+  const history = useNavigate();
 
   const handleSignIn = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
+    axios
+      .post(`${baseurl}/signin`, {
+        email: data.get("username"),
+        password: data.get("password"),
+      })
+      .then((res) => {
+        localStorage.setItem("AuthToken", res.data.token).console.log(res);
+      })
+      .catch((err) => console.log(err));
+    history("/dashboard");
+
     console.log({
       email: data.get("username"),
       password: data.get("password"),
